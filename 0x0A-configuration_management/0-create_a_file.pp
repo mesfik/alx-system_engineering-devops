7dee8d3d13d15be05676131265fa37e55c8a3cab
mesfik <fikreamesfin@gmail.com>
2023-06-23 22:57:38 +0300

    I love puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a file /tmp/school
 file { '/tmp/school':
-  ensure  => 'file',
-  path    => '/tmp/school'
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0774',
-  content => 'I love Puppet'
+    ensure  => 'file',
+    path    => '/tmp/school',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet'
 }
